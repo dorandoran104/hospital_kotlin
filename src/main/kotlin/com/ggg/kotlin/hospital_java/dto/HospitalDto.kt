31ab52e4ce5a8f8d56e60f96db265619ba9ce459
dorandoran104 <pgh_96@naver.com>
2024-10-22 15:07:51 +0900

    xml 파싱 완료

--- src/main/kotlin/com/ggg/kotlin/hospital_java/dto/HospitalDto.kt
+++ src/main/kotlin/com/ggg/kotlin/hospital_java/dto/HospitalDto.kt
@@ -4,36 +4,36 @@ data class HospitalDto(
     var rnum:Int
     ,var hpid:String
     ,var dutyName:String
-    ,var dutyAddr:String
-    ,var dutyDir:String
-    ,var dutyDirName:String
-    ,var dutyEmcls:String
-    ,var dutyEmclsName:String
-    ,var dutyEryn:Char
-    ,var dutyEtc:String
-    ,var dutyMapping:String
+    ,var dutyAddr:String?
+    ,var dutyDiv:String?
+    ,var dutyDivNam:String?
+    ,var dutyEmcls:String?
+    ,var dutyEmclsName:String?
+    ,var dutyEryn:String?
+    ,var dutyEtc:String?
+    ,var dutyMapimg:String?
     ,var dutyTel1:String
-    ,var dutyTel3:String
-    ,var dutyTime1c:String
-    ,var dutyTime2c:String
-    ,var dutyTime3c:String
-    ,var dutyTime4c:String
-    ,var dutyTime5c:String
-    ,var dutyTime6c:String
-    ,var dutyTime7c:String
-    ,var dutyTime8c:String
-    ,var dutyTime1s:String
-    ,var dutyTime2s:String
-    ,var dutyTime3s:String
-    ,var dutyTime4s:String
-    ,var dutyTime5s:String
-    ,var dutyTime6s:String
-    ,var dutyTime7s:String
-    ,var dutyTime8s:String
-    ,var postCdn1:String
-    ,var postCdn2:String
-    ,var wgs84Lon:String
-    ,var wgs84Lat:String
-    ,var dutyInf:String
-    ,var deleteYn:String
+    ,var dutyTel3:String?
+    ,var dutyTime1c:String?
+    ,var dutyTime2c:String?
+    ,var dutyTime3c:String?
+    ,var dutyTime4c:String?
+    ,var dutyTime5c:String?
+    ,var dutyTime6c:String?
+    ,var dutyTime7c:String?
+    ,var dutyTime8c:String?
+    ,var dutyTime1s:String?
+    ,var dutyTime2s:String?
+    ,var dutyTime3s:String?
+    ,var dutyTime4s:String?
+    ,var dutyTime5s:String?
+    ,var dutyTime6s:String?
+    ,var dutyTime7s:String?
+    ,var dutyTime8s:String?
+    ,var postCdn1:String?
+    ,var postCdn2:String?
+    ,var wgs84Lon:String?
+    ,var wgs84Lat:String?
+    ,var dutyInf:String?
+    ,var deleteYn:String? = "N"
 )
--- src/main/kotlin/com/ggg/kotlin/hospital_java/model/HospitalModel.kt
+++ src/main/kotlin/com/ggg/kotlin/hospital_java/model/HospitalModel.kt
@@ -1,5 +1,6 @@
 package com.ggg.kotlin.hospital_java.model
 
+import com.ggg.kotlin.hospital_java.dto.HospitalDto
 import org.apache.ibatis.session.SqlSession
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Repository
@@ -11,5 +12,9 @@ class HospitalModel {
     private lateinit var sqlSession: SqlSession
 
     private val HospitalNamespace = "HospitalMapper"
+
+    fun insertHospital(hospitalList: List<HospitalDto>){
+        sqlSession.insert(HospitalNamespace+".insert",hospitalList)
+    }
 
 }
